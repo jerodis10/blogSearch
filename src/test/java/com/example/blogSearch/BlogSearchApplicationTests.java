835ef6a7e09229d26dad401b3374ee6c9b8b567d
jerodis10 <jerodis10@naver.com>
2023-03-18 10:26:05 +0900

    Initial commit

---
+++ src/test/java/com/example/blogSearch/BlogSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
